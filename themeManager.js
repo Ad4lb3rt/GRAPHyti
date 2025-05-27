da91f0912911be80aaf5951bd3a6d51edf9b4003
Dxs7y <Jirka.d08@gmail.com>
2025-05-27 21:52:57 +0200

    a

--- themeManager.js
+++ themeManager.js
@@ -32,11 +32,21 @@ function changeSiteTheme(buttonIndex)
 {
     localStorage.setItem("currentSiteTheme", buttonIndex);
     currentSiteTheme = buttonIndex;
-    root.style.setProperty('--color-primary', getSiteColor(0));
-    root.style.setProperty('--color-background', getSiteColor(1));
-    root.style.setProperty('--color-foreground', getSiteColor(2));
-    root.style.setProperty('--color-button-hover', getSiteColor(3));
-    root.style.setProperty('--color-button-inactive', getSiteColor(4));
+    root.style.setProperty('--color-text-primary', getSiteColor(0));
+    root.style.setProperty('--color-text-secondary', getSiteColor(1));
+    root.style.setProperty('--color-text-tertiary', getSiteColor(2));
+    root.style.setProperty('--color-text-h2', getSiteColor(3));
+    root.style.setProperty('--color-text-h3', getSiteColor(4));
+    root.style.setProperty('--color-background-primary', getSiteColor(5));
+    root.style.setProperty('--color-background-secondary', getSiteColor(6));
+    root.style.setProperty('--color-teoria-alert-bg', getSiteColor(7));
+    root.style.setProperty('--color-teoria-example-bg', getSiteColor(8));
+    root.style.setProperty('--color-teoria-definition-bg', getSiteColor(9));
+    root.style.setProperty('--color-teoria-definition-outline', getSiteColor(10));
+    root.style.setProperty('--color-button-primary', getSiteColor(11));
+    root.style.setProperty('--color-button-secondary', getSiteColor(12));
+    root.style.setProperty('--color-button-hover', getSiteColor(13));
+    root.style.setProperty('--color-button-outline', getSiteColor(14));
 }
 
 function changeGraphTheme(buttonIndex)
